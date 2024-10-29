6dc4950a96843bb683a19692e3d40f6dc0b69c0e
Daniel Gomes <daniel167t@gmail.com>
2024-10-29 10:19:00 -0300

    feat: requisição da api via id para pagina about

--- src/app/pages/about-pokemon/about-pokemon.component.ts
+++ src/app/pages/about-pokemon/about-pokemon.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PokemonInfoService } from '../../shared/service/pokemon-info.service';
+import { IPokemonInfo } from '../../shared/models/pokemons-info';
+import { map, Observable, tap, of } from 'rxjs';
+import { FormatPokemonService } from '../../shared/service/format-pokemon.service';
 
 @Component({
   selector: 'app-about-pokemon',
@@ -7,20 +11,60 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './about-pokemon.component.scss',
 })
 export class AboutPokemonComponent implements OnInit {
-  constructor(private _activeRouter: ActivatedRoute) {}
-
   @Input() pokemonId?: string;
+  @Input() pokemonIdFormat?: string;
   @Input() pokemonName?: string;
   @Input() pokemonImage?: string;
   @Input() pokemonTypes?: { type: { name: string } }[];
   @Input() pokemonDesc?: string;
 
+  id: string = '';
+  pokemonInfo: Partial<IPokemonInfo> = {};
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _pokemonRest: PokemonInfoService,
+    private _formatPokemon: FormatPokemonService,
+  ) {}
+
   ngOnInit(): void {
-    this.getId;
+    this.getId();
+    this.getPokeInfo();
+  }
+
+  getPokeInfo() {
+    this._pokemonRest
+      .getPokemonById(this.id)
+      .pipe(
+        map(({ pokemon, descPokemon }) => {
+          return {
+            ...pokemon,
+            description: descPokemon.flavor_text_entries[0]?.flavor_text,
+          };
+        }),
+      )
+      .subscribe({
+        next: (pokemon: IPokemonInfo) => {
+          this.pokemonInfo = this.formatPokemonInfo(pokemon);
+          this.pokemonName = this.pokemonInfo.name;
+        },
+      });
+  }
+
+  formatPokemonInfo(pokemon: IPokemonInfo): IPokemonInfo {
+    let idFormat = pokemon.id;
+    while (idFormat.length < 3) {
+      idFormat = '0' + idFormat;
+    }
+    return {
+      ...pokemon,
+      idFormat: idFormat,
+      name: pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1).toLowerCase(),
+      description: pokemon.description?.replace(/[\n\f]/g, ''),
+    };
   }
 
   getId() {
-    const id = this._activeRouter.snapshot.params['id'];
-    console.log(id);
+    return (this.id = this._activatedRoute.snapshot.paramMap.get('id') ?? '');
   }
 }
