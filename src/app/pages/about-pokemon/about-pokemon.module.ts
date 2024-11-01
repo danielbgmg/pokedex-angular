3de265f4c71e1b6eeba95fa8d039d39d7e254474
Daniel Gomes <daniel167t@gmail.com>
2024-11-01 10:43:07 -0300

    feat: link para a home e estilo da pagina about

--- src/app/pages/about-pokemon/about-pokemon.module.ts
+++ src/app/pages/about-pokemon/about-pokemon.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { AboutPokemonComponent } from './about-pokemon.component';
 import { AboutRoutingModule } from './about-routing.module';
 
 @NgModule({
   declarations: [AboutPokemonComponent],
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule, NgOptimizedImage],
 })
 export class AboutPokemonModule {}
